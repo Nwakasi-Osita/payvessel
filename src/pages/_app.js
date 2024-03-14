f16f108ae1a3f036c03c7a9e7301878f5d8a2476
Nwakasi Osita <nwakasiosita081@gmail.com>
2024-03-14 07:27:37 +0100

    initialized git in this project

---
+++ src/pages/_app.js
@@ -0,0 +1,6 @@
+import "@/styles/globals.css";
+import "../../public/font-awesome/css/font-awesome.min.css";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
